de7b016a475d970103d7ea9289c2b35720982456
Josh Harbaugh <pixelhavenllc@gmail.com>
2025-07-13 21:47:30 -0400

    chore: added Authorization header to the /_cluster/health Elastic request.

--- api/search.js
+++ api/search.js
@@ -3,7 +3,14 @@ export default async function handler(req, res) {
   // TODO: Check if Elasticsearch is available
   try {
     const response = await fetch(
-      `${process.env.VITE_ELASTICSEARCH_URL}/_cluster/health`
+      `${process.env.VITE_ELASTICSEARCH_URL}/_cluster/health`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.VITE_ELASTICSEARCH_API_KEY}`,
+        },
+      }
     );
     const data = await response.json();
     res.json(data);
